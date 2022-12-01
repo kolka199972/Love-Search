2cb875a167804512be5552cfea9fadf3c8cc2453
kolka72 <kolia199972@gmail.com>
2022-12-01 23:13:10 +0300

    repeat

--- src/app/components/common/table/index.js
+++
@@ -1,7 +0,0 @@
-import Table from './table'
-import TableHeader from './tableHeader'
-import TableBody from './tableBody'
-
-export default Table
-
-export {TableHeader, TableBody}
--- src/app/utils/paginate.js
+++
@@ -1,4 +0,0 @@
-export const paginate = (items, currentNumber, pageSize) => {
-  const startIndex = (currentNumber - 1) * pageSize
-  return [...items].splice(startIndex, pageSize)
-}
